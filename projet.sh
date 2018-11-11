4bebd8d35543108855ed438cee79c5e43f7de21c
QuentinElsaesser <quent0205@hotmail.fr>
2018-11-11 09:44:42 +0100

    Ajout fct compare

--- projet.sh
+++ projet.sh
@@ -1,48 +1,50 @@
 #!/bin/bash
+test $# != 1 && echo "Pas assez de parametres donnés" && exit 1
 
-if [ $# != 1 ]
-then
-echo "pas de parametres"
-exit 1
-fi
-
-
+#Compare deux chaine donne en parametre
+function compare(){
+	ch1="$1"
+	ch2="$2"
+	if (test $ch1 \< $ch2)
+	then
+		echo "OK"
+	else
+		echo "KO"
+	fi
+}
 
 
 cherche () {
-total=""
-#local tag=$1
-local rep=$1
-cd ..
-#local name=$3
-#cd $rep
-#if [ -f $name -a $tag = "-f" ]
-#then
-#    echo "trouver fichier /$rep/$name"
-#    exit 1
-#elif [ -d $name -a $tag = "-d" ]
-#then
-#    echo "trouver dossier /$rep/$name"
-#    exit 1
-#fi
+	total=""
+	#local tag=$1
+	local rep=$1
+	#cd ..
+	#local name=$3
+	#cd $rep
+	#if [ -f $name -a $tag = "-f" ]
+	#then
+	#    echo "trouver fichier /$rep/$name"
+	#    exit 1
+	#elif [ -d $name -a $tag = "-d" ]
+	#then
+	#    echo "trouver dossier /$rep/$name"
+	#    exit 1
+	#fi
 
-for i in `find $rep`
-do
-    if [ $i != $rep ]
-    then
-#        local nrep="$rep/$i"
-    newname=`echo $i | sed "s#"$rep/"##g"`
-    total="$total""$newname;"
-#        cherche $tag $nrep $name
-    fi
-done
-
-echo "$total" | cut -d';' -f1
-var="$total" | cut -d';' -f1
-test=`echo "$total" | sed "s/$var//g"`
-echo "$test"
-
-
+	for i in `find $rep`
+	do
+		if [ $i != $rep ]
+		then
+	#        local nrep="$rep/$i"
+		newname=`echo $i | sed "s#"$rep/"##g"`
+		total="$total""$newname;"
+	#        cherche $tag $nrep $name
+		fi
+	done
+	echo "$total" | cut -d';' -f1
+	var="$total" | cut -d';' -f1
+	test=`echo "$total" | sed "s/$var//"`
+	echo "$test"
 }
 
 cherche $1
